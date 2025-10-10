0390ba98a8d9d21d5a63ed3005e675642e67d4bc
Emmanuel <emmanuel.martinez@ucsp.edu.pe>
2025-10-09 22:38:06 -0500

    env

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import { LabelStatsResponse } from '../dtos/label-stat-response.dto';
 import { ProcessedEmailsResponse } from '../dtos/processed-email-response.dto';
 
